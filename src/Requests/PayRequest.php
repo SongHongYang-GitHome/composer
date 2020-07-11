272a61fe95b01c19c60a2a0ab895bdd8aec9b9ea
宋杨 <945679940@qq.com>
2020-07-11 20:54:31 +0800

    test

--- src/Requests/PayRequest.php
+++ src/Requests/PayRequest.php
@@ -26,11 +26,11 @@ class PayRequest extends AbstractRequest
     // reqdata编码字符集，数字签名和base64编码中使用的字符集，仅支持 utf-8
     protected $charset = "utf-8";
     // 私钥
-    protected $privateKey = "MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBAJ7pGGqaT8DsbBZAKh/CPFIvtQVjDByCf0Wew3E8GiygEzmG/ZKPycFppEMbWL8XkZ3PMJb28y4NlvcRDD+z+PCakPTftfATnllJHdgv4K+YMLWrs4eYTbygpaXOv772DT7Y2g4Yyfk0UaRW9ex2Xv3NJ4p8kF3Gq1Y+lrOiaL5DAgMBAAECgYEAlJ4Tg7+FzzxNahHgU4totsR/W+ZiMVeqiAYPaxt2rz2WPTsOc7+eRRDz2v78ZNs6CIj5PHcAv5VeFvsOpZU/vuxPJ6uTvtZOYtM8YAeUbD2DXbP2CVC2DcDCQ7421hUbIlT0lqsCS4YaZgBsSt41GRd2fE8GlA4Aehs77qv+EUECQQDUK4S4wSctXblr6o6A+Vsj//kZWCUFc1DvmqNehsIQaKjWb15ehMlIirtYuWsEtJmkK4hMpuS2ih5qSjLFrTRZAkEAv7z7fjxMBf2O5lelJ1vLZiQ59bsTsFfyNDEzDbDxOBpnzDlaPXLrjf457SK2j8E/zbMZvNKfJv/SDU+enhhj+wJASu0zaauMklDO8nVa7eEhdo0nAvRF3q7injsWBoPAdNsBBPk/clGiY6PalXKlgHvm5jsZXzhw/KW6J/8b/wMzwQJAVOz75/oFIlIgzV3cxQYfnWpGUdU+70jE+uEf39yTu2nWt9pfYgBY3VfRiHtrqVjLTe0aZUxVOgztmP+/Hf9nqQJAME7Wjyu7hMkEiOnc3Ozgpd8rS2W4wXMOCdDkPtBvFf8bsvL/7+hejawc17NVR+/lCWrUQ3PDzpGUkgaxAu+PoA==";
+    protected $privateKey = "";
     // 公钥
-    protected $publicKey = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCe6Rhqmk/A7GwWQCofwjxSL7UFYwwcgn9FnsNxPBosoBM5hv2Sj8nBaaRDG1i/F5GdzzCW9vMuDZb3EQw/s/jwmpD037XwE55ZSR3YL+CvmDC1q7OHmE28oKWlzr++9g0+2NoOGMn5NFGkVvXsdl79zSeKfJBdxqtWPpazomi+QwIDAQAB";
+    protected $publicKey = "";
     // 兴业公钥
-    protected $XingyepublicKey = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCe6Rhqmk/A7GwWQCofwjxSL7UFYwwcgn9FnsNxPBosoBM5hv2Sj8nBaaRDG1i/F5GdzzCW9vMuDZb3EQw/s/jwmpD037XwE55ZSR3YL+CvmDC1q7OHmE28oKWlzr++9g0+2NoOGMn5NFGkVvXsdl79zSeKfJBdxqtWPpazomi+QwIDAQAB";
+    protected $XingyepublicKey = "";
 
     /**
      * @return string
